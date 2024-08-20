ebc23f3404fbd32b13a32367bc4abac527bb7022
patinor <jdieme27@gmail.com>
2024-08-20 17:50:59 +0000

    installation du templates

---
+++ resources/views/admin/pages/js.blade.php
@@ -0,0 +1,6 @@
+  <script src="/admin/vendors//admin/js/vendor.bundle.base.js"></script>
+  <script src="/admin/vendors/chart.js/Chart.min.js"></script>
+  <script src="/admin/js/off-canvas.js"></script>
+  <script src="/admin/js/hoverable-collapse.js"></script>
+  <script src="/admin/js/template.js"></script>
+  <script src="/admin/js/dashboard.js"></script>
---
+++ resources/views/admin/pages/sidebar.blade.php
@@ -0,0 +1,75 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+      <li class="nav-item sidebar-category">
+        <p>Navigation</p>
+        <span></span>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="mdi mdi-view-quilt menu-icon"></i>
+          <span class="menu-title">Dashboard</span>
+          <div class="badge badge-info badge-pill">2</div>
+        </a>
+      </li>
+      <li class="nav-item sidebar-category">
+        <p>Components</p>
+        <span></span>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="pages/forms/basic_elements.html">
+          <i class="mdi mdi-view-headline menu-icon"></i>
+          <span class="menu-title">Propriétaire</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/charts/chartjs.html">
+          <i class="mdi mdi-chart-pie menu-icon"></i>
+          <span class="menu-title">Catégorie</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/tables/basic-table.html">
+          <i class="mdi mdi-grid-large menu-icon"></i>
+          <span class="menu-title">Bien-immobilier</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/icons/mdi.html">
+          <i class="mdi mdi-emoticon menu-icon"></i>
+          <span class="menu-title">Client</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="pages/icons/mdi.html">
+          <i class="mdi mdi-emoticon menu-icon"></i>
+          <span class="menu-title">Contract</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="pages/icons/mdi.html">
+          <i class="mdi mdi-emoticon menu-icon"></i>
+          <span class="menu-title">Ventes</span>
+        </a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+          <i class="mdi mdi-account menu-icon"></i>
+          <span class="menu-title">User Pages</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="auth">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
+          </ul>
+        </div>
+      </li>
+      
+     
+    
+    </ul>
+  </nav>
